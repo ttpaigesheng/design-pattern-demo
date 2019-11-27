4f3ef0d68e3de9ed90acbd21e250cf3313f078a4
gesheng <gesheng@abupdate.com>
2019-11-27 20:58:10 +0800

    初始化

---
+++ src/main/java/com/abupdate/design/DesginPattenApplication.java
@@ -0,0 +1,16 @@
+package com.abupdate.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: sheng ge
+ * @Date: 2019/11/27 20:56
+ */
+@SpringBootApplication
+public class DesginPattenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesginPattenApplication.class, args);
+    }
+}
